61369459589f2abca49386bd799d29c61929161e
Gabe Underwood <Gabe.Underwood@vu.com>
2023-03-10 14:32:58 -0600

    try

--- oddAbsolute.py
+++ oddAbsolute.py
@@ -1,7 +1,22 @@
 def calculateAbsolute():
     
     # This first line is provided for you
-    in_num  = input("Enter a number: ")
+    try:
+        in_num = input("Enter a number: ")
+        in_num = int(in_num)
+    except:
+        print("bad number")
+
+    if in_num < 21:
+        in_num = 21 - in_num
+        print("Result:", in_num)
+    elif in_num > 21:
+        in_num = 2 * (in_num - 21)
+        print("Result:", in_num)
+    else:
+        print("Result:", '0')
+   
+    
     # end assignment
 
 ## If you want to test locally run > python payCalculator.py
